a99767854269bc5dbfc4687ee749a8f4fe46c42a
AminTaiTntra <amin.tai@tntra.io>
2026-05-22 11:54:21 +0530

    ecosystem updaed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,6 +6,10 @@ module.exports = {
       instances: 'max',
       exec_mode: 'cluster',
       env: {
+        NODE_ENV: 'development',
+        PORT: 3000
+      },
+      env_production: {
         NODE_ENV: 'production',
         PORT: 3000
       },
